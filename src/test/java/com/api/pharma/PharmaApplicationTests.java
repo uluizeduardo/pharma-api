e4af79cf58f8fc16c5bee9d00af5ff884305f108
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-01-15 23:37:53 -0300

    Project created

---
+++ src/test/java/com/api/pharma/PharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
