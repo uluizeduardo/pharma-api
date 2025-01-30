debfb521be59629c3cb9fad01a36fa8a0906c240
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-01-29 22:53:47 -0300

    feat: Created AuthenticationController class

---
+++ src/main/java/com/api/pharma/authentication/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.api.pharma.authentication.auth;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/api-pharma/auth",
+                consumes = {"application/json"},
+                produces = {"application/json"})
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody @Valid RegisterRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+}
