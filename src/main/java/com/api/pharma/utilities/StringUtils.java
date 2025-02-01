1ecfb1664081b07e47633c07a48dc81ac55559f7
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-01-31 23:53:48 -0300

    feat: Created constant that is used to define URLs that are freed (not protected) in security settings

---
+++ src/main/java/com/api/pharma/utilities/StringUtils.java
@@ -0,0 +1,13 @@
+package com.api.pharma.utilities;
+
+public class StringUtils {
+
+    public static final String[] WHITE_LIST_URL = {
+            "/v1/api-pharma/auth/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-resources/**"
+    };
+
+}
