df63bcc9ab89e3fa14dad0e928272da40d35ff09
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-02-02 21:00:46 -0300

    refactor: refactored saveUserToken method

--- src/main/java/com/api/pharma/authentication/auth/AuthenticationService.java
+++ src/main/java/com/api/pharma/authentication/auth/AuthenticationService.java
@@ -95,6 +95,8 @@ public class AuthenticationService {
                 .expired(false)
                 .revoked(false)
                 .build();
+
+        tokenRepository.save(token);
     }
 
     /**
