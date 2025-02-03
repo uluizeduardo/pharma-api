6fbbb9ee05c3e5bbf477656daafcfa15fb296ad1
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-02-02 21:03:26 -0300

    refactor: refactored RegisterRequest record

--- src/main/java/com/api/pharma/authentication/auth/RegisterRequest.java
+++ src/main/java/com/api/pharma/authentication/auth/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.api.pharma.authentication.auth;
 
 import com.api.pharma.model.enums.Role;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 
 public record RegisterRequest(
@@ -9,5 +10,5 @@ public record RegisterRequest(
         @NotBlank(message = "O Campo Email não pode ser vazio") String email,
         @NotBlank(message = "O Campo Senha não pode ser vazio") String password,
         boolean isActive,
-        @NotBlank(message = "O Campo Role não pode ser vazio") Role role) {
+        @NotNull(message = "O Campo Role não pode nulo") Role role) {
 }
