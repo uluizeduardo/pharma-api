4637b1459a05766c0c180577633fcfe73fad140b
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-02-02 21:13:24 -0300

    refactor: Added swagger documentation to registration method in AuthenticationControler

--- src/main/java/com/api/pharma/authentication/auth/AuthenticationController.java
+++ src/main/java/com/api/pharma/authentication/auth/AuthenticationController.java
@@ -1,16 +1,21 @@
 package com.api.pharma.authentication.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.web.bind.annotation.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "v1/api-pharma/auth",
-                consumes = {"application/json"},
+@RequestMapping(value = "/v1/api-pharma/auth",
                 produces = {"application/json"})
+@Tag(name = "Pharma API")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -20,9 +25,49 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody @Valid RegisterRequest request
-    ){
+    @Operation(
+            summary = "Performs the registration of new users",
+            description = "Registers a user by receiving their information, such as username, " +
+                          "email, password, activation status, and role."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User created successfully!",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AuthenticationResponse.class),
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                              "userId": "12345",
+                                              "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+                                            }
+                                            """
+                            )
+                    )),
+            @ApiResponse(responseCode = "400", description = "Invalid user data",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json"))
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "User registration request",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = """
+                                            {
+                                              "userName": "John Doe",
+                                              "email": "john.doe@example.com",
+                                              "password": "securePassword123",
+                                              "isActive": true,
+                                              "role": "PHARMACIST"
+                                            }
+                                            """
+                    )
+            )
+    )
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody @Valid RegisterRequest request) {
         return ResponseEntity.ok(authenticationService.register(request));
     }
+
 }
