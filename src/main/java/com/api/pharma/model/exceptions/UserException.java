b49cc9d9123614770bc1b169a8451115871ca38d
uluizeduardo <luiz.analistadesistemas@gmail.com>
2025-02-09 20:56:32 -0300

    refactor: Added message constants in specific constant class

--- src/main/java/com/api/pharma/model/exceptions/UserException.java
+++ src/main/java/com/api/pharma/model/exceptions/UserException.java
@@ -1,6 +1,7 @@
 package com.api.pharma.model.exceptions;
 
 import com.api.pharma.config.exceptionHandler.AlertException;
+import com.api.pharma.utilities.constants.MessageConstants.UserMessages;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
@@ -18,38 +19,32 @@ public class UserException extends AlertException {
                     LockedException.class, UserException::accountBlocked,
                     BadCredentialsException.class, UserException::badCredentials
             );
-    private static final String USER_NOT_FOUND  = "User not found!";
-    private static final String USER_NOT_FOUND_BY_EMAIL = "User not found to email: ";
-    private static final String ACCOUNT_DISABLED = "User account is disabled";
-    private static final String ACCOUNT_LOCKED = "User account is blocked";
-    private static final String INVALID_CREDENTIALS = "Bad credentials, Incorrect email or password.";
-    private static final String AUTHENTICATION_FAILED = "Authentication failed";
 
     public UserException(String code, String message, HttpStatus httpStatus) {
         super(code, message, httpStatus);
     }
 
     public static AlertException userNotFound() {
-        return new UserException("404", USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+        return new UserException("404", UserMessages.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
     public static AlertException userEmailNotFound(String email) {
-        return new UserException("404", USER_NOT_FOUND_BY_EMAIL + email, HttpStatus.NOT_FOUND);
+        return new UserException("404", UserMessages.USER_NOT_FOUND_BY_EMAIL + email, HttpStatus.NOT_FOUND);
     }
 
     public static AlertException accountDisabled() {
-        return new UserException("403", ACCOUNT_DISABLED, HttpStatus.FORBIDDEN);
+        return new UserException("403", UserMessages.ACCOUNT_DISABLED, HttpStatus.FORBIDDEN);
     }
 
     public static AlertException accountBlocked() {
-        return new UserException("403", ACCOUNT_LOCKED, HttpStatus.FORBIDDEN);
+        return new UserException("403", UserMessages.ACCOUNT_LOCKED, HttpStatus.FORBIDDEN);
     }
 
     public static AlertException badCredentials() {
-        return new UserException("401", INVALID_CREDENTIALS, HttpStatus.UNAUTHORIZED);
+        return new UserException("401", UserMessages.INVALID_CREDENTIALS, HttpStatus.UNAUTHORIZED);
     }
 
     public static AlertException authenticationFailed() {
-        return new UserException("401", AUTHENTICATION_FAILED, HttpStatus.UNAUTHORIZED);
+        return new UserException("401", UserMessages.AUTHENTICATION_FAILED, HttpStatus.UNAUTHORIZED);
     }
 
     public static AlertException fromAuthenticationFailed(AuthenticationException e) {
---
+++ src/main/java/com/api/pharma/utilities/constants/MessageConstants.java
@@ -0,0 +1,13 @@
+package com.api.pharma.utilities.constants;
+
+public class MessageConstants {
+
+    public static class UserMessages {
+        public static final String USER_NOT_FOUND = "User not found!";
+        public static final String USER_NOT_FOUND_BY_EMAIL = "User not found to email: ";
+        public static final String ACCOUNT_DISABLED = "User account is disabled";
+        public static final String ACCOUNT_LOCKED = "User account is blocked";
+        public static final String INVALID_CREDENTIALS = "Bad credentials, Incorrect email or password.";
+        public static final String AUTHENTICATION_FAILED = "Authentication failed";
+    }
+}
