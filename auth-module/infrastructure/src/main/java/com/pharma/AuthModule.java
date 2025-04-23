31887f186f1e93249952a3f4e5e6218816e3b0b8
uluizeduardo <luizprogramadorjava@gmail.com>
2025-04-22 21:37:18 -0300

    refactor(api): Implement clean architecture for the project
    - Refactored codebase to align with clean architecture principles.
    - Organized layers into domain, application, and infrastructure.
    - Updated Spring Boot configurations to support the new structure.
    - Adjusted Maven dependencies for modularization.

---
+++ auth-module/infrastructure/src/main/java/com/pharma/AuthModule.java
@@ -0,0 +1,12 @@
+package com.pharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthModule {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthModule.class, args);
+    }
+}
