35a1c307348d47df4f55199ae53319d3b7c6c8ed
uluizeduardo <luizprogramadorjava@gmail.com>
2025-04-24 22:46:27 -0300

    feat(auth): add Password value object for plain text password handling
    - Introduced `Password` to encapsulate plain text passwords.
    - Ensures validation of non-empty password values.
    - Provides a `getValue` method to retrieve the password securely.

---
+++ auth-module/domain/src/main/java/com/api/pharma/auth/valueobjects/password/Password.java
@@ -0,0 +1,33 @@
+package com.api.pharma.auth.valueobjects.password;
+
+/**
+ * Represents a text password that must be validated according to a PasswordPolicy
+ * before being accepted into the system.
+ *
+ * This Value Object is immutable and guarantees that any instance is valid.
+ */
+public class Password {
+
+    private String value;
+
+    public Password(String password, PasswordPolicy policy){
+        if(password == null || password.isBlank()){
+            throw new IllegalArgumentException("Password cannot be null or empty");
+        }
+        policy.validate(password);
+        this.value = password;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public HashedPassword hashWith(PasswordHasher hasher){
+        return hasher.hash(this);
+    }
+
+    @Override
+    public String toString() {
+        return "[PROTECTED]";
+    }
+}
