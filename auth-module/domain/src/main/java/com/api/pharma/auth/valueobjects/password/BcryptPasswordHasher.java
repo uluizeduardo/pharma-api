d8a4c85729e76425464ba6bf131040f93b7542be
uluizeduardo <luizprogramadorjava@gmail.com>
2025-04-24 22:40:40 -0300

    feat: implement BcryptPasswordHasher for secure password hashing
    - Added `BcryptPasswordHasher` class implementing the `PasswordHasher` interface.
    - Utilized Spring Security's `BCrypt` for password hashing and verification.
    - Ensures strong protection against brute force and rainbow table attacks.
    - Methods:
      - `hash(Password password)`: Hashes a plain text password using BCrypt.
      - `matches(Password password, HashedPassword hashedPassword)`: Verifies if a plain text password matches the hashed password.

---
+++ auth-module/domain/src/main/java/com/api/pharma/auth/valueobjects/password/BcryptPasswordHasher.java
@@ -0,0 +1,23 @@
+package com.api.pharma.auth.valueobjects.password;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+/**
+ * Implementation of PasswordHasher using the BCrypt algorithm.
+ *
+ * BCrypt automatically manages salting and provides strong protection
+ * against brute force and rainbow table attacks.
+ */
+public class BcryptPasswordHasher implements PasswordHasher{
+
+    @Override
+    public HashedPassword hash(Password password) {
+        String hashed = BCrypt.hashpw(password.getValue(), BCrypt.gensalt());
+        return new HashedPassword(hashed);
+    }
+
+    @Override
+    public boolean matches(Password password, HashedPassword hashedPassword) {
+        return BCrypt.checkpw(password.getValue(), hashedPassword.getValue());
+    }
+}
