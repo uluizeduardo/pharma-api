463176014414157f71d1d16c4b3c0c0803180035
uluizeduardo <luizprogramadorjava@gmail.com>
2025-05-02 23:47:59 -0300

    refactor(auth): Add isTokenValid and extractUsername in TokenServiceOutputPort interface to define token service output operations

--- auth-module/application/src/main/java/com/api/pharma/ports/out/TokenServiceOutputPort.java
+++ auth-module/application/src/main/java/com/api/pharma/ports/out/TokenServiceOutputPort.java
@@ -5,4 +5,6 @@ import com.api.pharma.entities.User;
 public interface TokenServiceOutputPort {
     String generateAccessToken(User user);
     String generateRefreshToken(User user);
+    boolean isTokenValid(String token, User user);
+    String extractUsername(String token);
 }
