97c08ba0d02111e44994cafb6bfaf2f3b94457cc
uluizeduardo <luizprogramadorjava@gmail.com>
2025-05-02 23:51:08 -0300

    feat(auth): Add TokenRepository to handle token persistence operations

---
+++ auth-module/infrastructure/src/main/java/com/api/pharma/persistence/repository/TokenRepository.java
@@ -0,0 +1,23 @@
+package com.api.pharma.persistence.repository;
+
+import com.api.pharma.persistence.entity.TokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
+
+
+    @Query(value = """
+            select t from TokenEntity t inner join UserEntity u \s
+            on t.userEntity.id = u.id \s
+            where t.expired = false and (t.expired = false or t.revoked = false) \s
+            """)
+    List<TokenEntity> findAllValidTokenByUser(Long userId);
+
+    Optional<TokenEntity> findByToken(String token);
+}
