50be4c5dad55b4fb62e60ae9b5c1328db81cfb5f
uluizeduardo <luizprogramadorjava@gmail.com>
2025-05-05 23:11:01 -0300

    feat(auth): Add `UserRepository` to manage user entity database operations

---
+++ auth-module/infrastructure/src/main/java/com/api/pharma/persistence/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.api.pharma.persistence.repository;
+
+import com.api.pharma.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
