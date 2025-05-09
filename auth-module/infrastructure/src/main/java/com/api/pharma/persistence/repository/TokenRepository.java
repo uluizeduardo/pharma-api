d4e9f763286886c9d29cad3832837eaf29693880
uluizeduardo <luizprogramadorjava@gmail.com>
2025-05-08 21:35:28 -0300

    feat(token): Change userId type in TokenRepository from Long to UUID for consistency

--- auth-module/infrastructure/src/main/java/com/api/pharma/persistence/repository/TokenRepository.java
+++ auth-module/infrastructure/src/main/java/com/api/pharma/persistence/repository/TokenRepository.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
+public interface TokenRepository extends JpaRepository<TokenEntity, UUID> {
 
 
     @Query(value = """
@@ -17,7 +18,7 @@ public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
             on t.userEntity.id = u.id \s
             where t.expired = false and (t.expired = false or t.revoked = false) \s
             """)
-    List<TokenEntity> findAllValidTokenByUser(Long userId);
+    List<TokenEntity> findAllValidTokenByUser(UUID userId);
 
     Optional<TokenEntity> findByToken(String token);
 }
