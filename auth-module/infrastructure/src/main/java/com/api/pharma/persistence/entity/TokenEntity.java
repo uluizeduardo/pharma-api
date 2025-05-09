37be62be46bde4777e8a0db08b4928887480e972
uluizeduardo <luizprogramadorjava@gmail.com>
2025-05-08 21:35:18 -0300

    feat(token): Change id type in TokenEntity from Long to UUID for better uniqueness

--- auth-module/infrastructure/src/main/java/com/api/pharma/persistence/entity/TokenEntity.java
+++ auth-module/infrastructure/src/main/java/com/api/pharma/persistence/entity/TokenEntity.java
@@ -10,6 +10,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
 
 @Data
 @Builder
@@ -17,14 +19,14 @@ import java.io.Serial;
 @AllArgsConstructor
 @Entity
 @Table(name = "tb_tokens")
-public class TokenEntity {
+public class TokenEntity implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     @Column(unique = true)
     private String token;
