b8b5cf1693b69d11b61fe5ffe94837c08492ea3f
uluizeduardo <luizprogramadorjava@gmail.com>
2025-05-08 21:34:17 -0300

    feat(auth): Update registerUser method to return AuthenticationResponse instead of RegisterUserResponse

--- auth-module/application/src/main/java/com/api/pharma/ports/in/RegisterUserInputPort.java
+++ auth-module/application/src/main/java/com/api/pharma/ports/in/RegisterUserInputPort.java
@@ -1,8 +1,8 @@
 package com.api.pharma.ports.in;
 
+import com.api.pharma.dto.AuthenticationResponse;
 import com.api.pharma.dto.RegisterUserRequest;
-import com.api.pharma.dto.RegisterUserResponse;
 
 public interface RegisterUserInputPort {
-    RegisterUserResponse registerUser(RegisterUserRequest registerUserRequest);
+    AuthenticationResponse registerUser(RegisterUserRequest registerUserRequest);
 }
