6b4b34b047dd1001caf1dff6b9fe49f9f94c7514
uluizeduardo <luizprogramadorjava@gmail.com>
2025-08-26 21:53:05 -0300

    feat(auth): Deleted EmailAlreadyInUseException class

--- src/main/java/com/api/pharma/modules/auth_module/application/exceptions/EmailAlreadyInUseException.java
+++
@@ -1,7 +0,0 @@
-package com.api.pharma.modules.auth_module.application.exceptions;
-
-public class EmailAlreadyInUseException extends RuntimeException {
-    public EmailAlreadyInUseException(String message) {
-        super(message);
-    }
-}
